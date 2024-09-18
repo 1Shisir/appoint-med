f7e2896d60ce47db4c565598373f4093354ad1cc
1Shisir <87371639+1Shisir@users.noreply.github.com>
2024-09-18 14:29:57 +0545

    about-us-page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
             <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
             <ul className='flex flex-col gap-2 text-gray-600'>
                 <li>+977 98xxxxxxx</li>
-                <li>example.info@gmail.com</li>
+                <li>appointmed.info@gmail.com</li>
             </ul>
 
         </div>
